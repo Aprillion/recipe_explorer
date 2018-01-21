0e76c0cfa64c6d474c90869c61f8e354acf21ec0
Peter Hozak <peter.hozak@gmail.com>
2018-01-21 19:17:54 +0100

    export crafting machines
    i.e. entity_prototypes with the crafting_categories property

--- dump_json.lua
+++ dump_json.lua
@@ -13,30 +13,36 @@ end
 local table_to_json
 local node_to_json
 
-table_to_json = function(node, ignored_indexes, max_level, level, first, last, is_array)
+table_to_json = function(node, mandatory_index, ignored_indexes, max_level, level, first, last, is_array)
   local out = ''
   local i = 0
   for k, v in pairs(node) do
-    i = i + 1
-    if i == 1 then
-      out = out .. "\n"
-    else
-      out = out .. ",\n"
+    local matching_mandatory = true
+    if mandatory_index and not v[mandatory_index] then
+      matching_mandatory = false
     end
-    out = out .. string.rep(' ', level * 2)
-    if is_array then
-      out = out .. node_to_json(v, ignored_indexes, max_level, level)
-    else
-      out = out .. '"' .. k .. '": '
-      if ignored_indexes[k] then
-        out = out .. '"<ignored>"'
+    if matching_mandatory then
+      i = i + 1
+      if i == 1 then
+        out = out .. "\n"
       else
-        local ignored_indexes_next = {}
-        for ig_k, ig_v in pairs(ignored_indexes) do
-          ignored_indexes_next[ig_k] = ig_v
+        out = out .. ",\n"
+      end
+      out = out .. string.rep(' ', level * 2)
+      if is_array then
+        out = out .. node_to_json(v, nil, ignored_indexes, max_level, level)
+      else
+        out = out .. '"' .. k .. '": '
+        if ignored_indexes[k] then
+          out = out .. '"<ignored>"'
+        else
+          local ignored_indexes_next = {}
+          for ig_k, ig_v in pairs(ignored_indexes) do
+            ignored_indexes_next[ig_k] = ig_v
+          end
+          ignored_indexes_next[k] = true
+          out = out .. node_to_json(v, nil, ignored_indexes_next, max_level, level)
         end
-        ignored_indexes_next[k] = true
-        out = out .. node_to_json(v, ignored_indexes_next, max_level, level)
       end
     end
   end
@@ -46,12 +52,12 @@ table_to_json = function(node, ignored_indexes, max_level, level, first, last, i
   return first .. out .. last
 end
 
-node_to_json = function(node, ignored_indexes, max_level, level)
+node_to_json = function(node, mandatory_index, ignored_indexes, max_level, level)
   level = level or 0
   local out = ''
   if type(node) == "table" then
     if #node > 0 then
-      out = out .. table_to_json(node, ignored_indexes, max_level, level + 1, '[', ']', true)
+      out = out .. table_to_json(node, mandatory_index, ignored_indexes, max_level, level + 1, '[', ']', true)
     else
       if node.__self then
         if level <= max_level then
@@ -65,7 +71,7 @@ node_to_json = function(node, ignored_indexes, max_level, level)
           node = {["<parsing limit reached>"] = max_level}
         end
       end
-      out = out .. table_to_json(node, ignored_indexes, max_level, level + 1, '{', '}', false)
+      out = out .. table_to_json(node, nil, ignored_indexes, max_level, level + 1, '{', '}', false)
     end
   elseif type(node) == "string" then
     out = out .. '"' .. node:gsub('"', '`') .. '"'
@@ -90,8 +96,11 @@ local game_settings = {
     mod_settings = game.player.mod_settings
   }
 }
-game.write_file(dir .. "/game.json", node_to_json(game_settings, {}, 2))
+game.write_file(dir .. "/game.json", node_to_json(game_settings, nil, {}, 2))
 
 local ignored_indexes = {force=true, prototype=true, group=true, subgroup=true}
-game.write_file(dir .. "/game.player.force.recipes.json", node_to_json(game.player.force.recipes, ignored_indexes, 4))
-game.write_file(dir .. "/game.player.force.technologies.json", node_to_json(game.player.force.technologies, ignored_indexes, 2))
+game.write_file(dir .. "/game.player.force.recipes.json", node_to_json(game.player.force.recipes, nil, ignored_indexes, 4))
+game.write_file(dir .. "/game.player.force.technologies.json", node_to_json(game.player.force.technologies, nil, ignored_indexes, 2))
+
+local mandatory_index = 'crafting_categories'
+game.write_file(dir .. "/game.entity_prototypes.json", node_to_json(game.entity_prototypes, mandatory_index, ignored_indexes, 2))
